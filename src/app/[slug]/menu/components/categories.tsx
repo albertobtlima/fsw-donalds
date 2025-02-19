28c5e325ef51b155387c8ac1a2dc7550f6ae79b8
Alberto Lima <alberto.btlima@outlook.com>
2025-02-19 12:39:58 -0300

    up date

---
+++ src/app/[slug]/menu/components/categories.tsx
@@ -0,0 +1,53 @@
+import { Prisma } from "@prisma/client";
+import { ClockIcon } from "lucide-react";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+
+interface RestaurantCategoriesProps {
+  restaurant: Prisma.RestaurantGetPayload<{
+    include: {
+      menuCategories: {
+        include: { products: true };
+      };
+    };
+  }>;
+}
+
+const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
+  return (
+    <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl border bg-white">
+      <div className="flex items-center gap-3">
+        <Image
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+          width={45}
+          height={45}
+        />
+        <div>
+          <h2 className="text-lg font-semibold">{restaurant.name}</h2>
+          <p className="text-xs opacity-55">{restaurant.description}</p>
+        </div>
+      </div>
+      <div className="mt-3 flex items-center gap-1 text-xs text-green-500">
+        <ClockIcon size={12} />
+        <p>Aberto!</p>
+      </div>
+
+      <ScrollArea>
+        <div className="flex w-max space-x-4">
+          {restaurant.menuCategories.map(category => (
+            <Button key={category.id} variant="secondary" size="sm">
+              {category.name}
+            </Button>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+      
+    </div>
+  );
+};
+
+export default RestaurantCategories;
--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import RestaurantCategories from "./components/categories";
 import RestaurantHeader from "./components/header";
 
 interface RestaurantMenuPageProps {
@@ -24,7 +25,11 @@ const RestaurantMenuPage = async ({
     return notFound();
   }
 
-  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  const restaurant = await db.restaurant.findUnique({
+    where: { slug },
+    include: { menuCategories: { include: { products: true } } },
+  });
+
   if (!restaurant) {
     return notFound();
   }
@@ -33,6 +38,7 @@ const RestaurantMenuPage = async ({
     <div>
       <div className="relative h-[250px] w-full">
         <RestaurantHeader restaurant={restaurant} />
+        <RestaurantCategories restaurant={restaurant} />
       </div>
     </div>
   );
